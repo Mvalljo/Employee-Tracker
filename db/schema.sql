9e0488175c05fbdfb8de67d585d99d2ba9b9eb86
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-10-14 02:24:53 -0500

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,10 @@ CREATE TABLE role(
     id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 DROP TABLE IF EXISTS employee;
@@ -23,5 +26,11 @@ CREATE TABLE employee(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
