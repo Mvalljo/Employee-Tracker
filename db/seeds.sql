51d15846d0f5475e86f24bce8c0b1ddcf6cc3f87
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-10-14 20:25:44 -0500

    added comments and more info, ordered employee's

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,13 +3,20 @@ VALUES ("Engineering"),
        ("Finance"),
        ("Legal"),
        ("Sales");
+/*view all departments*/
+SELECT *
+FROM department;
 
 INSERT INTO role (title,salary,department_id)
-VALUES ("Sales Lead","100000",4),
-       ("Accountant","125000",2),
-       ("Lawyer","190000",3),
-       ("Software Engineer","120000",1);
-
+VALUES  ("Sales Lead","100000",4),
+        ("Salesperson","80000",4),
+        ("Account Manager","160000",2),
+        ("Accountant","125000",2),
+        ("Legal Team Lead","250000",3),
+        ("Lawyer","190000",3),
+        ("Lead Engineer","150000",1),
+        ("Software Engineer","120000",1);
+/*view all roles*/
 SELECT 
     role.id,
     role.title,
@@ -17,14 +24,19 @@ SELECT
     role.salary
 FROM role
     JOIN department
-    ON role.department_id = department.id;
+    ON role.department_id = department.id
+ORDER BY role.id;
 
 INSERT INTO employee (first_name,last_name,role_id,manager_id)
-VALUES ("John","Doe",1,null),
-       ("Malia","Brown",2,1),
-       ("Kevin","Tupick",4,1),
-       ("Tom","Allen",3,1);
-       
+VALUES  ("Naruto","Uzumaki",1,null),   
+        ("John","Doe",2,2),
+        ("Bruce","Wayne",7,null),
+        ("Asami","Sato",8,3),
+        ("Kento","Nanami",3,null),
+        ("Jordan","Belfort",4,5),
+        ("Loki","Laufeyson",5,null),
+        ("Tom","Allen",6,7);
+/*view all employees*/
 SELECT 
     employee.id,
     employee.last_name,
@@ -39,4 +51,7 @@ FROM employee
     INNER JOIN role 
     ON employee.role_id = role.id 
     INNER JOIN department 
-    ON role.department_id = department.id;
+    ON role.department_id = department.id
+ORDER BY employee.id;
+
+
