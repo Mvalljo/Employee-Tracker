fa0af2eff942ae6a2ed4bbf8964e4d6df73afcf3
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-10-19 02:35:34 -0500

    moved prompt questions to a new file

---
+++ questions.js
@@ -0,0 +1,195 @@
+const mysql = require('mysql2');
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: process.env.DB_PASSWORD,
+        database: 'employee_tracker_db'
+    },
+);
+
+const starterQ = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['View all employees', 'Add an employee',
+            'Update an employee role', 'Update an employees manager',
+            'View employees by manager', 'View employees by department',
+            'View all roles', 'Add a role',
+            'View all departments', 'Add a department',
+            'Delete a department', 'Delete a role', 'Delete a employee',
+            'View the total utilized budget of each department',
+            'Quit'],
+        name: 'choice',
+    },
+];
+
+
+const addDepartmentQ = [
+    {
+        type: 'input',
+        message: 'What is the name of the department?',
+        name: 'departmentName',
+    }
+]
+
+//Gets roles and puts it in a array
+const emplRole = new Array;
+db.query('SELECT role.title FROM role', function (err, results) {
+    for (let i = 0; i < results.length; i++) {
+        emplRole.push(results[i].title);
+    }
+});
+//Gets managers and puts it in a array
+const emplMang = ["None"];
+db.query("SELECT concat(employee.first_name, ' ' ,  employee.last_name) AS employee, concat(manager.first_name, ' ' ,  manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id INNER JOIN role ON employee.role_id = role.id;", function (err, results) {
+    for (let i = 0; i < results.length; i++) {
+        if (results[i].manager === null) {
+            emplMang.push(results[i].employee);
+        }
+    }
+});
+//Array of questions to add an employee
+const addEmployeeQ = [
+    {
+        type: 'input',
+        message: 'What is the employees first name?',
+        name: 'employeeFirstName',
+    },
+    {
+        type: 'input',
+        message: 'What is the employees last name?',
+        name: 'employeeLastName',
+    },
+    {
+        type: 'list',
+        message: 'What is the employees role?',
+        //Get roles from database
+        choices: emplRole,
+        name: 'employeeRole',
+    },
+    {
+        type: 'list',
+        message: 'What is the employees manager?',
+        //Get managers name from database
+        choices: emplMang,
+        name: 'employeeManager',
+    }
+]
+
+//Gets department names and puts it in a array
+const dept = new Array;
+db.query('SELECT department.name FROM department', function (err, results) {
+    for (let i = 0; i < results.length; i++) {
+        dept.push(results[i].name);
+    }
+});
+//Array of questions to add a role
+const addRoleQ = [
+    {
+        type: 'input',
+        message: 'What is the name of the role?',
+        name: 'roleName',
+    },
+    {
+        type: 'input',
+        message: 'What is the salary of the role?',
+        name: 'roleSalary',
+    },
+    {
+        type: 'list',
+        message: 'Which department does the role belong to?',
+        //Get departments from database
+        choices: dept,
+        name: 'roleDepartment'
+    }
+]
+
+//Gets employee's name and puts it in a array
+const emplNames = new Array;
+db.query("SELECT concat(employee.first_name, ' ' ,  employee.last_name) AS employee FROM employee", function (err, results) {
+    for (let i = 0; i < results.length; i++) {
+        emplNames.push(results[i].employee);
+    }
+});
+//Array of questions to update an employee
+const updateEmployee = [
+    {
+        type: 'list',
+        message: 'Which employees role do you want to update?',
+        //Get employees names from database
+        choices: emplNames,
+        name: 'updateName'
+    },
+    {
+        type: 'list',
+        message: 'Which role do you wnat to assign the selected employee?',
+        //Get employess role from database
+        choices: emplRole,
+        name: 'updateRole'
+    }
+]
+
+//Array of questions to update an employee's manager
+const updateEmployeeM = [
+    {
+        type: 'list',
+        message: "Which employee's manager do you want to update?",
+        //Get employees names from database
+        choices: emplNames,
+        name: 'updateNameM'
+    },
+    {
+        type: 'list',
+        message: 'Which manager do you wnat to assign the selected employee?',
+        //Get employess role from database
+        choices: emplMang,
+        name: 'updateManager'
+    }
+]
+
+//Array of question to choose what department to delete
+const deleteDepartment = [
+    {
+        type: 'list',
+        message: "What department do you want to delete?",
+        //Get employeres names from database
+        choices: dept,
+        name: 'deletedDept'
+    }
+]
+
+//Array of question to choose what role to delete
+const deleteRole = [
+    {
+        type: 'list',
+        message: "What role do you want to delete?",
+        //Get employees names from database
+        choices: emplRole,
+        name: 'deletedRole'
+    }
+]
+
+//Array of question to choose which employee to delete
+const deleteEmployee = [
+    {
+        type: 'list',
+        message: "Which employee do you want to delete?",
+        //Get employees names from database
+        choices: emplNames,
+        name: 'deletedEmpl'
+    }
+]
+
+exports.starterQ = starterQ;
+exports.addDepartmentQ = addDepartmentQ;
+exports.addEmployeeQ = addEmployeeQ;
+exports.addRoleQ = addRoleQ;
+exports.updateEmployee = updateEmployee;
+exports.updateEmployeeM = updateEmployeeM;
+exports.deleteDepartment = deleteDepartment;
+exports.deleteRole = deleteRole;
+exports.deleteEmployee = deleteEmployee;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('dotenv').config();
+const questions = require('./questions');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -14,191 +15,19 @@ const db = mysql.createConnection(
     },
 );
 
-const starterQ = [
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: ['view all department', 'add a department',
-            'view all employees', 'add an employee',
-            'update an employee role', 'update an employees manager',
-            'view employees by manager', 'view employees by department',
-            'view all roles', 'add a role',
-            'Delete a department', 'Delete a role', 'Delete a employee',
-            'View the total utilized budget of each department',
-            'Quit'],
-        name: 'choice',
-    },
-];
-
-const addDepartmentQ = [
-    {
-        type: 'input',
-        message: 'What is the name of the department?',
-        name: 'departmentName',
-    }
-]
-
-//Gets roles and puts it in a array
-const emplRole = new Array;
-db.query('SELECT role.title FROM role', function (err, results) {
-    for (let i = 0; i < results.length; i++) {
-        emplRole.push(results[i].title);
-    }
-});
-//Gets managers and puts it in a array
-const emplMang = ["None"];
-db.query("SELECT concat(employee.first_name, ' ' ,  employee.last_name) AS employee, concat(manager.first_name, ' ' ,  manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id INNER JOIN role ON employee.role_id = role.id;", function (err, results) {
-    for (let i = 0; i < results.length; i++) {
-        if (results[i].manager === null) {
-            emplMang.push(results[i].employee);
-        }
-    }
-});
-//Array of questions to add an employee
-const addEmployeeQ = [
-    {
-        type: 'input',
-        message: 'What is the employees first name?',
-        name: 'employeeFirstName',
-    },
-    {
-        type: 'input',
-        message: 'What is the employees last name?',
-        name: 'employeeLastName',
-    },
-    {
-        type: 'list',
-        message: 'What is the employees role?',
-        //Get roles from database
-        choices: emplRole,
-        name: 'employeeRole',
-    },
-    {
-        type: 'list',
-        message: 'What is the employees manager?',
-        //Get managers name from database
-        choices: emplMang,
-        name: 'employeeManager',
-    }
-]
-
-//Gets department names and puts it in a array
-const dept = new Array;
-db.query('SELECT department.name FROM department', function (err, results) {
-    for (let i = 0; i < results.length; i++) {
-        dept.push(results[i].name);
-    }
-});
-//Array of questions to add a role
-const addRoleQ = [
-    {
-        type: 'input',
-        message: 'What is the name of the role?',
-        name: 'roleName',
-    },
-    {
-        type: 'input',
-        message: 'What is the salary of the role?',
-        name: 'roleSalary',
-    },
-    {
-        type: 'list',
-        message: 'Which department does the role belong to?',
-        //Get departments from database
-        choices: dept,
-        name: 'roleDepartment'
-    }
-]
-
-//Gets employee's name and puts it in a array
-const emplNames = new Array;
-db.query("SELECT concat(employee.first_name, ' ' ,  employee.last_name) AS employee FROM employee", function (err, results) {
-    for (let i = 0; i < results.length; i++) {
-        emplNames.push(results[i].employee);
-    }
-});
-//Array of questions to update an employee
-const updateEmployee = [
-    {
-        type: 'list',
-        message: 'Which employees role do you want to update?',
-        //Get employees names from database
-        choices: emplNames,
-        name: 'updateName'
-    },
-    {
-        type: 'list',
-        message: 'Which role do you wnat to assign the selected employee?',
-        //Get employess role from database
-        choices: emplRole,
-        name: 'updateRole'
-    }
-]
-
-//Array of questions to update an employee's manager
-const updateEmployeeM = [
-    {
-        type: 'list',
-        message: "Which employee's manager do you want to update?",
-        //Get employees names from database
-        choices: emplNames,
-        name: 'updateNameM'
-    },
-    {
-        type: 'list',
-        message: 'Which manager do you wnat to assign the selected employee?',
-        //Get employess role from database
-        choices: emplMang,
-        name: 'updateManager'
-    }
-]
-
-//Array of question to choose what department to delete
-const deleteDepartment = [
-    {
-        type: 'list',
-        message: "What department do you want to delete?",
-        //Get employeres names from database
-        choices: dept,
-        name: 'deletedDept'
-    }
-]
-
-//Array of question to choose what role to delete
-const deleteRole = [
-    {
-        type: 'list',
-        message: "What role do you want to delete?",
-        //Get employees names from database
-        choices: emplRole,
-        name: 'deletedRole'
-    }
-]
-
-//Array of question to choose which employee to delete
-const deleteEmployee = [
-    {
-        type: 'list',
-        message: "Which employee do you want to delete?",
-        //Get employees names from database
-        choices: emplNames,
-        name: 'deletedEmpl'
-    }
-]
-
 function init() {
     inquirer
-        .prompt(starterQ)
+        .prompt(questions.starterQ)
         .then((data) => {
-            if (data.choice === "view all department") {
+            if (data.choice === "View all departments") {
                 // Query database
                 db.query('SELECT department.name FROM department', function (err, results) {
                     console.table(results);
                     init();
                 });
-            } else if (data.choice === "add a department") {
+            } else if (data.choice === "Add a department") {
                 inquirer
-                    .prompt(addDepartmentQ)
+                    .prompt(questions.addDepartmentQ)
                     .then((data) => {
                         db.query(`INSERT INTO department (name) VALUES (?);`, data.departmentName, (err, results) => {
                             if (err) {
@@ -210,15 +39,15 @@ function init() {
                             return results;
                         });
                     })
-            } else if (data.choice === "view all employees") {
+            } else if (data.choice === "View all employees") {
                 // Query database
                 db.query("SELECT employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, concat(manager.first_name, ' ' ,  manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id;", function (err, results) {
                     console.table(results);
                     init();
                 });
-            } else if (data.choice === "add an employee") {
+            } else if (data.choice === "Add an employee") {
                 inquirer
-                    .prompt(addEmployeeQ)
+                    .prompt(questions.addEmployeeQ)
                     .then((data) => {
                         db.query("SELECT * FROM role;", function (err, results) {
                             //Searches for the role name that matches the answer given to get the role id
@@ -252,9 +81,9 @@ function init() {
                             });
                         });
                     })
-            } else if (data.choice === "update an employee role") {
+            } else if (data.choice === "Update an employee role") {
                 inquirer
-                    .prompt(updateEmployee)
+                    .prompt(questions.updateEmployee)
                     .then((data) => {
                         db.query("SELECT id, concat(employee.first_name, ' ' ,  employee.last_name) AS employee FROM employee;", function (err, results) {
                             //Searches for the employee name that matches the answer given to get the employee id
@@ -285,9 +114,9 @@ function init() {
                             });
                         });
                     })
-            } else if (data.choice === "update an employees manager") {
+            } else if (data.choice === "Update an employees manager") {
                 inquirer
-                    .prompt(updateEmployeeM)
+                    .prompt(questions.updateEmployeeM)
                     .then((data) => {
                         db.query("SELECT id, concat(employee.first_name, ' ' ,  employee.last_name) AS employee FROM employee;", function (err, results) {
                             //Searches for the employee name that matches the answer given to get the employee id
@@ -319,7 +148,7 @@ function init() {
                             });
                         });
                     });
-            } else if (data.choice === "view employees by manager") {
+            } else if (data.choice === "View employees by manager") {
                 // Query database
                 db.query("SELECT concat(manager.first_name, ' ' ,  manager.last_name) AS manager, GROUP_CONCAT(employee.first_name,' ',employee.last_name) AS employees FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id WHERE employee.manager_id!='NULL' GROUP BY manager;", function (err, results) {
                     if (err) {
@@ -329,7 +158,7 @@ function init() {
                     }
                     init();
                 });
-            } else if (data.choice === "view employees by department") {
+            } else if (data.choice === "View employees by department") {
                 // Query database
                 db.query("SELECT department.name AS department, GROUP_CONCAT(employee.first_name,' ',employee.last_name) AS employees FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id GROUP BY department;", function (err, results) {
                     if (err) {
@@ -339,15 +168,15 @@ function init() {
                     }
                     init();
                 });
-            } else if (data.choice === "view all roles") {
+            } else if (data.choice === "View all roles") {
                 // Query database
                 db.query("SELECT role.title, department.name as department, role.salary FROM role JOIN department ON role.department_id = department.id;", function (err, results) {
                     console.table(results);
                     init();
                 });
-            } else if (data.choice === "add a role") {
+            } else if (data.choice === "Add a role") {
                 inquirer
-                    .prompt(addRoleQ)
+                    .prompt(questions.addRoleQ)
                     .then((data) => {
                         db.query("SELECT * FROM Department;", function (err, results) {
                             //Searches for the department name that matches the answer given to get the department id
@@ -372,7 +201,7 @@ function init() {
                     })
             } else if (data.choice === "Delete a department") {
                 inquirer
-                    .prompt(deleteDepartment)
+                    .prompt(questions.deleteDepartment)
                     .then((data) => {
                         db.query(`DELETE FROM department WHERE name = "?";`, data.deletedDept, (err, results) => {
                             if (err) {
@@ -386,7 +215,7 @@ function init() {
                     })
             } else if (data.choice === "Delete a role") {
                 inquirer
-                    .prompt(deleteRole)
+                    .prompt(questions.deleteRole)
                     .then((data) => {
                         db.query(`DELETE FROM role WHERE title = "?";`, data.deletedRole, (err, results) => {
                             if (err) {
@@ -400,7 +229,7 @@ function init() {
                     })
             } else if (data.choice === "Delete a employee") {
                 inquirer
-                    .prompt(deleteEmployee)
+                    .prompt(questions.deleteEmployee)
                     .then((data) => {
                         db.query("SELECT id, concat(employee.first_name, ' ' ,  employee.last_name) AS employee FROM employee;", function (err, results) {
                             //Searches for the employee name that matches the answer given to get the employee id
